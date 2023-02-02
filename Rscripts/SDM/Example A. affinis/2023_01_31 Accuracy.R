f3c91618959dd1ce9a84d9f98ae7725bba88bffd
Etienne Fort <etienne.fort@etu.univ-amu.fr>
2023-02-02 13:47:25 +0100

    Reorganisation2

--- Rscripts/SDM/Example A. affinis/2023_01_31 Accuracy.R
+++
@@ -1,3 +0,0 @@
-# Performance des algo, EF, 31/01/2023
-
-load(file="data_SDM_examples/accuracy/ALdrovandia_affinis_accuracy.Rdata")
--- Rscripts/SDM/Example A. affinis/2023_01_31 Modeles.R
+++
@@ -1,3 +0,0 @@
-# distribution de proba de presence selon les algo
-
-load(file="data_SDM_examples/modeles/Aldrovandia_affinis_brt2.Rdata")
